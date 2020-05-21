cf1d3602a7fb9a0178660577fb3b61ef9eff6d27
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2020-05-21 22:16:36 +0300

    Finish resize functional

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,35 @@
 import {ExcelComponent} from '@core/ExelComponent';
 import {createTable} from './table.template';
+import {tableResizeHandler} from './table.resize';
+import {shouldResize} from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excell__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable()
   }
+
+  // onClick() {
+  //   console.log('onClick')
+  // }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      tableResizeHandler(this.$root, event)
+    }
+  }
+  // onMousemove() {
+  //   console.log('onMousemove')
+  // }
+
+  // onMouseup() {
+  //   console.log('onMouseup')
+  // }
 }

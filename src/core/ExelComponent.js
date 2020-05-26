0a7aa0621fd8e1d8e1b0a7f98dfca5849fcee1f0
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2020-05-26 22:39:12 +0300

    Finish redux and table logic

--- src/core/ExelComponent.js
+++ src/core/ExelComponent.js
@@ -1,10 +1,12 @@
 import {DomListener} from '@core/DomListener'
+
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
-    console.log(options)
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
 
     this.prepare()
@@ -15,6 +17,10 @@ export class ExcelComponent extends DomListener {
   // Return layout component
   toHTML() {
     return ''
+  }
+  // Сюда приходят только изменения по тем полям на которые мы подписались
+  storeChanged() {
+
   }
   // Инициализируем компонент
   // добавляем ДОМСлушателей
@@ -29,6 +35,14 @@ export class ExcelComponent extends DomListener {
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn)
     this.unsubscribers.push(unsub)
+  }
+  // For redux
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   // Удаляем компонент
